c788693e91808652c9a22ac71ee26e190210863a
shiningsudipto <shiningsudipto.com>
2025-08-29 01:33:34 +0600

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import TopBar from "../components/shared/TopBar";
+
+const page = () => {
+  return (
+    <div>
+      <TopBar />
+    </div>
+  );
+};
+
+export default page;
---
+++ src/components/shared/TopBar.tsx
@@ -0,0 +1,14 @@
+const TopBar = () => {
+  return (
+    <div className="h-[42px] bg-primary text-white">
+      <div className="flex items-center h-full border border-white border-t-0 border-b-0 px-4 w-fit space-x-4">
+        <p>Terms & Conditions</p>
+        <p>Privacy Policy</p>
+        <p>Refund & Returns Policy</p>
+      </div>
+      <div></div>
+    </div>
+  );
+};
+
+export default TopBar;
