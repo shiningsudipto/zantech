7f98d8d902b8ddc6e3f75fbfcb4d24b5085cec74
shiningsudipto <shiningsudipto.com>
2025-09-06 00:47:41 +0600

    working on filter

--- src/app/(public)/products/page.tsx
+++ src/app/(public)/products/page.tsx
@@ -1,7 +1,14 @@
+import Filter from "@/components/shared/Filter";
+
 const page = () => {
   return (
-    <div>
-      <p>Hello, page!</p>
+    <div className="section-gap py-20 grid grid-cols-10 gap-5">
+      <div className="col-span-2">
+        <Filter />
+      </div>
+      <div className="col-span-8 bg-red-100">
+        <p>Products</p>
+      </div>
     </div>
   );
 };
---
+++ src/components/shared/Filter.tsx
@@ -0,0 +1,72 @@
+"use client";
+
+import { useState } from "react";
+import { Slider } from "../ui/slider";
+import { Checkbox } from "@/components/ui/checkbox";
+import { Label } from "../ui/label";
+import { useRouter, useSearchParams } from "next/navigation";
+
+const productStatus = [
+  {
+    label: "On sale",
+    value: "onsale",
+  },
+  {
+    label: "In stock",
+    value: "instock",
+  },
+];
+
+const Filter = () => {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+
+  const [value, setValue] = useState([0, 90]);
+  const [selectedStatus, setSelectedStatus] = useState<string[]>([]);
+  const toggleStatus = (status: string) => {
+    setSelectedStatus((prev) =>
+      prev.includes(status)
+        ? prev.filter((s) => s !== status)
+        : [...prev, status]
+    );
+  };
+
+  const handleSort = (key: string, value: string) => {
+    const params = new URLSearchParams(searchParams.toString());
+    params.set(key, value);
+    router.push(`?${params.toString()}`);
+  };
+
+  return (
+    <div className="">
+      <div className="space-y-2">
+        <p className="text-lg font-bold">Filter by Price</p>
+        <div className="flex items-center justify-between gap-2 font-semibold">
+          <p>{value[0]}</p>
+          <p>{value[1] * 200}</p>
+        </div>
+        <Slider
+          value={value}
+          onValueChange={setValue}
+          aria-label="Dual range slider with output"
+        />
+      </div>
+      <div className="border-t mt-10"></div>
+      <div className="flex flex-col gap-y-4 mt-7">
+        <p className="text-lg font-bold">Stock status</p>
+        {productStatus.map((item, idx) => (
+          <div key={idx} className="flex items-center space-x-2">
+            <Checkbox
+              id={item.value}
+              checked={selectedStatus.includes(item.value)}
+              onCheckedChange={() => toggleStatus(item.value)}
+            />
+            <Label htmlFor={item.value}>{item.label}</Label>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Filter;
