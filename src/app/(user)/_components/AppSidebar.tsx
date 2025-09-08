52512efc7959b04fdd7569b59544ddc22cf38634
shiningsudipto <shiningsudipto.com>
2025-09-08 22:59:25 +0600

    user dashboard setup done

---
+++ src/app/(user)/_components/AppSidebar.tsx
@@ -0,0 +1,99 @@
+"use client";
+import { Calendar, Home, Inbox, Search, Settings } from "lucide-react";
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  useSidebar,
+} from "@/components/ui/sidebar";
+import Link from "next/link";
+import { useAuthStore } from "@/stores/authStore";
+import { usePathname } from "next/navigation";
+
+const items = [
+  {
+    title: "Dashboard",
+    url: "/user",
+    icon: Home,
+  },
+  {
+    title: "Orders",
+    url: "#",
+    icon: Inbox,
+  },
+  {
+    title: "Downloads",
+    url: "#",
+    icon: Calendar,
+  },
+  {
+    title: "Addresses",
+    url: "#",
+    icon: Search,
+  },
+  {
+    title: "Account Details",
+    url: "#",
+    icon: Settings,
+  },
+];
+
+const AppSidebar = () => {
+  const { state } = useSidebar();
+  const { user } = useAuthStore();
+  const pathname = usePathname();
+  return (
+    <Sidebar collapsible="icon" className="text-white">
+      <SidebarContent className="bg-primary">
+        <SidebarGroup>
+          <SidebarGroupLabel className="text-white p-4 mb-4 mt-2 border border-blue-300">
+            <div className="text-center w-full">
+              <h2 className="text-xl">{user?.name}</h2>
+              <p className="text-sm">{user?.email}</p>
+            </div>
+          </SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {items.map((item) => {
+                const isActive = pathname === item?.url;
+                return (
+                  <SidebarMenuItem
+                    key={item.title}
+                    className="flex items-center justify-center"
+                  >
+                    <SidebarMenuButton asChild className="">
+                      <Link
+                        href={item.url}
+                        className={`${
+                          isActive && "bg-secondary flex justify-center"
+                        }`}
+                      >
+                        <item.icon className={`${isActive && "ms-2.5"}`} />
+                        <span className="text-base">{item.title}</span>
+                      </Link>
+                    </SidebarMenuButton>
+                  </SidebarMenuItem>
+                );
+              })}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+      <SidebarFooter className="bg-primary">
+        {state === "collapsed" ? (
+          <span className="text-sm font-bold">Z</span>
+        ) : (
+          <span className="text-sm font-bold">Zantech</span>
+        )}
+      </SidebarFooter>
+    </Sidebar>
+  );
+};
+
+export default AppSidebar;
--- src/app/(user)/layout.tsx
+++ src/app/(user)/layout.tsx
@@ -1,7 +1,19 @@
 import { ReactNode } from "react";
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+import AppSidebar from "./_components/AppSidebar";
 
 const layout = ({ children }: { children: ReactNode }) => {
-  return <main>{children}</main>;
+  return (
+    <SidebarProvider className="">
+      <AppSidebar />
+      <main className="w-full">
+        <div className="bg-primary h-10 flex items-center">
+          <SidebarTrigger className="text-2xl hover:text-black" />
+        </div>
+        {children}
+      </main>
+    </SidebarProvider>
+  );
 };
 
 export default layout;
